eb4c2ddd6d298aec5287f2accfaf064f0c78deb4
JunHaoShih <victor5517@gmail.com>
2023-07-13 08:43:43 +0800

    fix: add missing files

---
+++ SprmApi/Common/Authorizations/RequirePermissionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SprmApi.Core.ObjectTypes;
+
+namespace SprmApi.Common.Authorizations
+{
+    /// <summary>
+    /// 要求指定權限
+    /// </summary>
+    public class RequirePermissionAttribute : TypeFilterAttribute
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="types"></param>
+        public RequirePermissionAttribute(params SprmObjectType[] types) : base(typeof(RequireBaseAttribute))
+        {
+            Arguments = new object[] { types };
+        }
+    }
+}
